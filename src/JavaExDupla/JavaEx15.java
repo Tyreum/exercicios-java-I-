ce10aa11e41ed385ae647e39be69be4969736658
Tyreum <gabriel.kmn@gmail.com>
2019-09-25 01:08:43 -0300

    exercicio 15

---
+++ src/JavaExDupla/JavaEx15.java
@@ -0,0 +1,42 @@
+
+package JavaExDupla;
+
+import javax.swing.*;
+public class JavaEx15 {
+    
+    public static void main(String[] args) {
+        int refeicao = 0, prato = 0, opcoes = 0, calorias = 0;
+        String comida = "";
+        try{
+        
+            comida = JOptionPane.showInputDialog(null, "Escolha uma das refeições da lista\n\n1 - Prato: Vegetariano, Sobremesa: Abacaxi, Bebida: Chá\n2 - Prato: Peixe, Sobremesa: Sorvete diet, Bebida: Suco de laranja\n3 - Prato: Frango, Sobremesa: Mousse diet, Bebida: Suco de melão\n 4 - Prato: Carne, Sobremesa: Mousse de chocolate, Bebida: Refrigerante diet\n\n");
+            switch(prato){
+                
+                case 1: comida = "Prato: Vegetariano, Sobremesa: Abacaxi, Bebida: Chá ";
+                        calorias = 275;
+                        JOptionPane.showMessageDialog(null, "A refeição que escolheu contém: "+ calorias +"kcal");
+                        break;
+                 
+                case 2: comida = "Prato: Peixe, Sobremesa: Sorvete diet, Bebida: Suco de laranja";
+                        calorias = 470;
+                        JOptionPane.showMessageDialog(null, "A refeição que escolheu contém: "+ calorias +"kcal");
+                        break;
+                        
+                case 3: comida = "Prato: Frango, Sobremesa: Mousse diet, Bebida: Suco de melão";
+                        calorias = 520;
+                        JOptionPane.showMessageDialog(null, "A refeição que escolheu contém: "+ calorias +"kcal");
+                        break;
+                        
+                case 4: comida = "Prato: Carne, Sobremesa: Mousse de chocolate, Bebida: Refrigerante diet";
+                        calorias = 615;
+                        break;
+            }
+            JOptionPane.showMessageDialog(null, "A refeição que escolheu contém: "+ calorias +"kcal");
+        }
+        catch (NumberFormatException | NullPointerException ex) {
+            JOptionPane.showMessageDialog(null, "inserir apenas valores numéricos");
+        }
+        
+    }
+    
+}
